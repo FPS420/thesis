04fb89f6da4318d08a89bee5f5028542ea4375ed
fps420 <schoenknecht.florian@gmail.com>
2022-09-01 10:02:36 +0200

    create Token.sol

--- Example_Token_Ecosystem/contracts/Token.sol
+++ Example_Token_Ecosystem/contracts/Token.sol
@@ -5,7 +5,7 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
 
-    constructor() ERC20("Lotte Coin", "LTT") {
-        _mint(msg.sender,1234 * 10 ** 18);
+    constructor() ERC20("Lotte", "LTT") {
+        _mint(msg.sender,1000000 * 10 ** 18);
     }
 }
