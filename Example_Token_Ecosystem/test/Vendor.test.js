84f34eac237bdd901d09f840ce663ed94cb0684c
fps420 <schoenknecht.florian@gmail.com>
2022-09-04 11:54:49 +0200

    implement test for minting Tokens

--- Example_Token_Ecosystem/test/Vendor.test.js
+++ Example_Token_Ecosystem/test/Vendor.test.js
@@ -166,11 +166,23 @@ chai.expect();
         it("Top up Vendor.sol with ETH successfully",async()=>{
             assert.equal(web3.utils.fromWei(await web3.eth.getBalance(vendorAddress)),0);
             await web3.eth.sendTransaction({to: vendorAddress, from: deployer, value:web3.utils.toWei("0.5","ether")});
+            assert.equal(web3.utils.fromWei(await web3.eth.getBalance(vendorAddress)),0.5);
         });
-        it("Mint 20% of total supply",async()=>{
+        it("Mint 20% of total supply successfully",async()=>{
+            let cap = web3.utils.fromWei(await contractToken.methods.cap().call());
+            
             balanceVendor = await contractToken.methods.balanceOf(vendorAddress).call();
             assert.equal(web3.utils.fromWei(balanceVendor),0);
-            //await contractToken.methods.
+            
+            await contractToken.methods.mint(cap/5,vendorAddress).send({from: deployer});
+            
+            balanceVendor = await contractToken.methods.balanceOf(vendorAddress).call();
+            assert.equal(web3.utils.fromWei(balanceVendor), cap/5);
+        });
+        it("Mint more than cap should rejected",async()=>{
+            let totalSupply = web3.utils.fromWei(await contractToken.methods.totalSupply().call());
+            let cap = web3.utils.fromWei(await contractToken.methods.cap().call());
+            await contractToken.methods.mint((cap-totalSupply)+1,vendorAddress).send({from: deployer}).should.be.rejected;
         });
     });
   });
