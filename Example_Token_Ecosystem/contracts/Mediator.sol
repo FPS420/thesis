a5213e13dbfa45bad1d6509c8d8f4190823f457d
fps420 <schoenknecht.florian@gmail.com>
2022-09-05 11:49:13 +0200

    create and get Job & testing it

--- Example_Token_Ecosystem/contracts/Mediator.sol
+++ Example_Token_Ecosystem/contracts/Mediator.sol
@@ -3,10 +3,36 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./Token.sol";
 contract Mediator {
     Token public token;
-    
+    mapping(uint256 => Job) public jobs;
+    uint256 public lastId = 0;
+
     constructor(address tokenAddress) {
     token = Token(tokenAddress);
     }
 
-    
+    struct Job {
+        uint256 id;
+        string title;
+        address client;
+        address contractor;
+        string description;
+        uint256 remuneration;
+    }
+
+    function createId() private returns (uint256){
+        lastId = lastId +1;
+        return lastId;
+    }
+
+    function createJob(string memory description, uint256 remuneration, string memory title) public returns(Job memory){
+        uint256 _id = createId();
+        Job memory newJob = Job(_id, title, msg.sender, msg.sender, description,remuneration);
+        jobs[_id]= newJob;
+        return newJob;
+    }
+
+    function getJobById(uint256 _id) public view returns(Job memory){
+        return jobs[_id];
+    }
+
 }
--- Example_Token_Ecosystem/test/Mediator.test.js
+++ Example_Token_Ecosystem/test/Mediator.test.js
@@ -1,9 +1,219 @@
 const Mediator = artifacts.require("Mediator.sol");
-
-contract("Mediator",([deployer,third])=>{
+const abiMediator = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "tokenAddress",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "remuneration",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "title",
+				"type": "string"
+			}
+		],
+		"name": "createJob",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "id",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "title",
+						"type": "string"
+					},
+					{
+						"internalType": "address",
+						"name": "client",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "contractor",
+						"type": "address"
+					},
+					{
+						"internalType": "string",
+						"name": "description",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "remuneration",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Mediator.Job",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "getJobById",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "id",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "title",
+						"type": "string"
+					},
+					{
+						"internalType": "address",
+						"name": "client",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "contractor",
+						"type": "address"
+					},
+					{
+						"internalType": "string",
+						"name": "description",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "remuneration",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Mediator.Job",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "jobs",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "title",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "client",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "contractor",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "remuneration",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "token",
+		"outputs": [
+			{
+				"internalType": "contract Token",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+contract("Mediator",([client,contractor])=>{
     before(async() => {
         mediator = await Mediator.deployed();
         mediatorAddress = await mediator.address;
-        //contractMediator = new web3.eth.Contract(abiVendor,mediatorAddress);
+        contractMediator = new web3.eth.Contract(abiMediator,mediatorAddress);
         });
+    describe('deployment',async()=>{
+        it('deploys successfully', async ()=> {
+            assert.notEqual(mediatorAddress, 0x0)
+            assert.notEqual(mediatorAddress, '')
+            assert.notEqual(mediatorAddress, null)
+            assert.notEqual(mediatorAddress, undefined)
+        });
+    });
+    describe('List a Job',async()=>{
+        it('listing successfully', async ()=> {
+            let id = await contractMediator.methods.lastId().call();
+            await contractMediator.methods.createJob("You have to learn Solidity",10, "Learning Solidity").send({from: client, gas: 200000});
+            let job = await contractMediator.methods.getJobById(id).call();
+            assert.equal(id, job.id);
+        });
+    });        
 });
