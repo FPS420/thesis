4f8067e729e23cddf79e2623c1e5419cbbeb30e5
fps420 <schoenknecht.florian@gmail.com>
2022-09-21 11:23:11 +0200

    comment erc20 and er721 standard

--- ERC.X/test/ERC721Standard.test.js
+++ ERC.X/test/ERC721Standard.test.js
@@ -31,33 +31,44 @@ contract("ERC721",([deployer,buyer,seller])=>{
             assert.equal(balanceDeployer, totalSupply);
         })
     })
+    // Test Mint Function
     describe('Mint',async()=>{  
         it('mint new Tokens',async()=>{
+            //to mint a new deed, it have to pay a fee of 0.05 ether
             value = web3.utils.toWei('0.05');
+            //seller is calling mint function 
             await erc721.methods.createNewNFT().send({from:seller, value:value, gas:200000});
+            //supply has to be increased
             supply = await erc721.methods.totalSupply().call();
             assert.equal(supply,totalSupply+1);
         })
     })
+    // Test Access 
     describe('Access Managment',async()=>{  
         it('check Owner',async()=>{
+            // owner of deed 0 is deployer, owner of deed 1 should be seller
             owner = await erc721.methods.ownerOf(0).call();
             owner1 = await erc721.methods.ownerOf(1).call();
             assert.equal(deployer,owner);
             assert.equal(seller,owner1);
         })
     })
+    // approve seller to transer deployer deed 0 to buyer
     describe('Approve',async()=>{  
         it('Approve seller to transfer deployers deed',async()=>{
             await erc721.methods.approve(seller,0).send({from:deployer});
         })
     })
+    // Test Transfering of deeds
     describe('Transfer',async()=>{  
+         // seller transfer deed 1 to buyer
         it('seller transfer deed 1 to buyer',async()=>{
             await erc721.methods.transferFrom(seller,buyer,1).send({from:seller, gas:2000000});
             buyerBalance= await erc721.methods.balanceOf(buyer).call();
             assert.equal(buyerBalance,1);
         })
+        // seller is approved to transfer deployers deed 0 to buyer
+        // buyer should hold total supply
         it('seller transfer deed 0 from deployer to buyer',async()=>{
             await erc721.methods.transferFrom(deployer,buyer,0).send({from:seller, gas:2000000});
             buyerBalance= await erc721.methods.balanceOf(buyer).call();
